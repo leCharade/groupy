35f35ef73d0f578a29c9d63310c960c665c56058
Tom Tournillon <tom.tournillon@gmail.com>
2022-11-09 01:44:47 +0100

    Correctifs

--- front/src/components/DeletePost.js
+++ front/src/components/DeletePost.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 
 export default function DeletePost(props) {
     const { deletePost, handleDisableDeletePost } = props;
@@ -10,10 +9,10 @@ export default function DeletePost(props) {
         let ca = decodedCookie.split(';');
         for(let i = 0; i <ca.length; i++) {
           let c = ca[i];
-          while (c.charAt(0) == ' ') {
+          while (c.charAt(0) === ' ') {
             c = c.substring(1);
           }
-          if (c.indexOf(name) == 0) {
+          if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
           }
         }
